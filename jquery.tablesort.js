28238216aaf5a2b5808fc7a5ac55200470525b19
Lubber <c64@lubber.de>
2021-10-14 22:03:20 +0200

    fix(compare): support pure number comparison

--- jquery.tablesort.js
+++ jquery.tablesort.js
@@ -107,6 +107,10 @@
 		asc: 'sorted ascending',
 		desc: 'sorted descending',
 		compare: function(a, b) {
+			if(!isNaN(a) && !isNaN(b)){
+				a = parseInt(a);
+				b = parseInt(b);
+			}
 			if (a > b) {
 				return 1;
 			} else if (a < b) {
